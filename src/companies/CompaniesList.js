758a424e3fbc3c9665f74ce4324b8e766ef62e8f
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-03-08 20:46:00 -0600

    Add Link component around each CompanyCard component

--- src/companies/CompaniesList.js
+++ src/companies/CompaniesList.js
@@ -1,17 +1,25 @@
 import React from "react";
 import CompanyCard from "./CompanyCard";
+import { Link } from "react-router-dom";
+import "./CompaniesList.css";
 
 const CompaniesList = ({ companies }) => {
   return (
     <div className="CompaniesList">
-      {companies.map(c => 
-        (<CompanyCard 
-          key={c.handle}
-          handle={c.handle}
-          name={c.name} 
-          description={c.description}
-        />)
-      )}
+      {companies.map(c => {
+        return (
+          <Link 
+            to={`/companies/${c.handle}`} 
+            key={c.handle} 
+            className="CompaniesList-Link"
+          >
+            <CompanyCard 
+              name={c.name} 
+              description={c.description}
+            />
+          </Link>
+        )
+      })}
     </div>
   )
 }
