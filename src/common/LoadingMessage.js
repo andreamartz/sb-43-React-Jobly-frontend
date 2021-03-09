f08e5181015a991b1ce902fafc5aa4cfb7d6d245
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-03-08 20:38:49 -0600

    Add Loading Message component and its styles

---
+++ src/common/LoadingMessage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoadingMessage.css";
+
+/** Loading message used by components that fetch API data. */
+
+function LoadingMessage() {
+  return (
+      <div className="LoadingMessage">
+        Loading ...
+      </div>
+  );
+}
+
+export default LoadingMessage;
