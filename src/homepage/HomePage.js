b3e778b1f45e250b1013aad38eb12aef36d38161
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-03-14 16:16:00 -0500

    Change home page based on logged in status

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,11 +1,43 @@
-import React from "react";
+/** HomePage
+ * 
+ *  Purpose: 
+ *  - show welcome message
+ *  - message should include name of logged in user (if one exists)
+ * 
+ *  Props: none
+ * 
+ *  State: 
+ * 
+ */
+import React, { useContext } from "react";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import UserContext from "../auth/UserContext";
+import "./HomePage.css";
 
 function HomePage() {
+  const { currentUser } = useContext(UserContext);
 
   return (
-    <>
-     <p>HOME PAGE</p>
-    </>
+    <div className="HomePage">
+      <div>
+        <h1>Jobly</h1>
+        <p>All the jobs in one, convenient place.</p>
+        {/* Logged in user? Show username
+        Otherwise, show login and signup buttons */}
+        {currentUser
+          ? <h2>Welcome Back, {`${currentUser.firstName}`}</h2>
+          : <p>
+              <Link to="/login">
+                <Button color="primary" className="HomePage-Button">Log in</Button>
+              </Link>
+              <Link to="/signup">
+                <Button color="primary" className="HomePage-Button">Sign up</Button>
+              </Link>
+            </p>
+        }
+      </div>
+    </div>
   )
 }
 
