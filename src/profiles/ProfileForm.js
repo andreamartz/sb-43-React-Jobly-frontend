d4a4f4a1ec82516ef3c4cd8778636b95582017bc
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-03-16 17:04:19 -0500

    Add basic profile form

--- src/profiles/ProfileForm.js
+++ src/profiles/ProfileForm.js
@@ -1,11 +1,129 @@
-import React from "react";
+/** SignupForm
+ * 
+ * Props: none
+ * 
+ * State:
+ * - form data
+ */
 
-function ProfileForm() {
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import "./ProfileForm.css";
+
+const ProfileForm = () => {
+  const [form, setForm] = useState({
+    username: "", 
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setForm(f => ({
+      ...f,
+      [name]: value
+    }));
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const { 
+      username, 
+      firstName,
+      lastName,
+      email,
+      password
+    } = form;
+  }
 
   return (
-    <>
-      <p>PROFILE FORM</p>
-    </>
+    <div className="ProfileForm">
+      <h1>Profile</h1>
+      <div className="ProfileForm-FormContainer">
+        <Form
+          className="ProfileForm-Form"
+          onSubmit={handleSubmit}
+        >
+          <FormGroup>
+            <Label>
+              Username
+            </Label>
+            <Input 
+              type="text"
+              name="username"
+              id="username"
+              value={form.username}
+              onChange={handleChange}
+              bsSize="md"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              First name
+            </Label>
+            <Input 
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={form.firstName}
+              onChange={handleChange}
+              bsSize="md"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              Last name
+            </Label>
+            <Input 
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+              bsSize="md"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              Email
+            </Label>
+            <Input 
+              type="text"
+              name="email"
+              id="email"
+              value={form.email}
+              onChange={handleChange}
+              bsSize="md"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              Confirm password to make changes:
+            </Label>
+            <Input 
+              type="text"
+              name="password"
+              id="password"
+              value={form.password}
+              onChange={handleChange}
+              bsSize="md"
+            />
+          </FormGroup>
+          <div className="ProfileForm-SubmitButton">
+            <Button 
+              type="submit" 
+              color="primary"
+              size="md"
+              block
+            >
+              Save Changes
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
   )
 }
 
