6432255fb86b75da7bfdbad4e26c550b260ae1c8
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-03-16 11:20:47 -0500

    Create PrivateRoute to control access to certain routes

---
+++ src/routes-and-nav/PrivateRoute.js
@@ -0,0 +1,33 @@
+/** PrivateRoute component
+ * 
+ * Purpose: create a route component that only logged in users can access
+ * 
+ */
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import UserContext from "../auth/UserContext";
+
+const PrivateRoute = ({ exact, path, children }) => {
+  const { currentUser } = useContext(UserContext);
+
+  console.debug(
+    "PrivateRoute",
+    "exact=", exact,
+    "path=", path,
+    "currentUser=", currentUser,
+  );
+
+  if (!currentUser) {
+    return (
+      <Redirect to="/login" />
+    )
+  }
+
+  return (
+    <Route exact={exact} path={path}>
+      {children}
+    </Route>
+  )
+}
+
+export default PrivateRoute;
